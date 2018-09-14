fbd24b29f6ab749ad7e0a7844d6ba8a0a58209d3
georg90 <Georg.online@posteo.de>
2018-09-14 17:22:20 +0200

    fixes problems with multiple ip per interface

--- lumi-aqara/index.js
+++ lumi-aqara/index.js
@@ -40,11 +40,17 @@ var Aqara = function (_events$EventEmitter) {
         var _iteratorError = undefined;
 
         try {
+          var i = 0;
           for (var _iterator = networkIface[Symbol.iterator](), _step; !(_iteratorNormalCompletion = (_step = _iterator.next()).done); _iteratorNormalCompletion = true) {
-            var connection = _step.value;
 
+            var connection = _step.value;
             if (connection.family === 'IPv4') {
-              _this._serverSocket.addMembership(MULTICAST_ADDRESS, connection.address);
+              console.log(MULTICAST_ADDRESS)
+              console.log(connection)
+              if(i == 0){
+                _this._serverSocket.addMembership(MULTICAST_ADDRESS, connection.address);
+                i++;
+              }
             }
           }
         } catch (err) {
